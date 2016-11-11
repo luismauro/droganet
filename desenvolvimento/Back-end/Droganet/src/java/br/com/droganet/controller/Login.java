b35bd27c6f21c6337ba7475468a5386e75feab1c
luismauro <luispenholato@hotmail.com>
2016-11-11 02:29:51 -0200

    Login

---
+++ desenvolvimento/Back-end/Droganet/src/java/br/com/droganet/controller/Login.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.droganet.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author luis mauro
+ */
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+      String login = null;
+      String senha = null;
+      
+      login = request.getParameter("login");
+      senha = request.getParameter("senha");
+     
+      request.setAttribute("LoginATT",login);
+      request.setAttribute("SenhaATT",senha);
+     
+      if(!login.isEmpty() && !senha.isEmpty()){
+          
+         switch(login) {
+         case "admin" :
+             request.getRequestDispatcher("HomeSuperAdmin.jsp").forward(request, response);
+            break;
+         case "funcionario" :
+             request.getRequestDispatcher("HomeFuncionario.jsp").forward(request, response);
+            break;
+         case "gerente" :
+             request.getRequestDispatcher("HomeGerenteLoja.jsp").forward(request, response);
+            break;
+      }
+          
+           
+      }
+      
+      else
+      {
+          System.out.println("Preencha todos os campos");
+      }
+      
+      
+      
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
